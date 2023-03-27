4734b5f10ce8f2474981b472c0e2fbd6e3d5a6e5
paulo-zx <paulo-zx@hotmail.com>
2023-03-27 20:52:01 -0300

    rest api carro

---
+++ src/test/java/com/example/apicarroteste/ApiCarroTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicarroteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarroTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
